7f8ee762199b3dd4ffff976d487a80e09469d449
nhaterik <nhaterik@gmail.com>
2024-04-30 17:05:31 +0700

    add all files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,31 @@
-// Importing createStore from the Redux package hosted on Skypack
-import { createStore } from 'https://cdn.skypack.dev/redux';
-
-// Reducer function to handle state changes
-function reducer(state = 0, action) {
-    switch (action.type) {
-        case 'increment':
-            return state + action.payload;
-        case 'decrease':
-            return state - action.payload;
-        default:
-            return state;
+const Btn=function({title,link,click}) {
+    let Component='button'
+    let obj={}
+    if(link)
+    {
+        Component='a'
+        obj.href=link
     }
+    if(click)
+    {
+        
+        obj.onClick=click
+    }
+    return (
+        <Component {...obj}> 
+            {title}
+        </Component>
+    )
 }
-
-// Create a Redux store with the reducer
-const store = createStore(reducer);
-// Action creators
-function increment(payload) {
-    return { type: 'increment', payload  };
+function App(){
+   return (
+    <React.Fragment>
+        <Btn
+        title="click"
+        link='fb.com'
+        click={()=>console.log('ok')}
+        />
+    </React.Fragment>
+   )
 }
-
-function decrease(payload) {
-    return { type: 'decrease', payload  };
-}
-document.querySelector('.deposit').onclick= ()=> {
-    store.dispatch(increment(10))
-    render()
-}
-document.querySelector('.withdraw').onclick= ()=> {
-    store.dispatch(decrease(10))
-    render()
-}
-function render() {
-    document.querySelector('.output').innerText=store.getState()
-}
-render()
-
+ReactDOM.render(<App/>,document.querySelector('#root'))
